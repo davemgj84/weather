7182d599035c046a44bef2bf3c86d3fb9c7790b0
David <davemgj@gmail.com>
2020-12-21 23:08:12 -0800

    create dynamic input for weather data + more styling

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import currentDate from "../helpers/currentDate";
 import "../styles/Weather.scss";
 
 const Weather = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      axios
+        .get(`/api/weather/${query}`)
+        .then((response) => response.data)
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+        });
+    }
+  };
+
   return (
     <>
       <main>
@@ -11,9 +28,36 @@ const Weather = () => {
             type="text"
             className="search-bar"
             placeholder="Search for a city..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
         <h2>Weather</h2>
+        {weather.city !== undefined ? (
+          <div>
+            <div className="info-container">
+              <div className="location-box">
+                <div className="location">
+                  {weather.city}, {weather.country}
+                </div>
+                <div className="date">{currentDate(new Date())}</div>
+              </div>
+              <div className="weather-box">
+                <div className="temp">{weather.temperature}°c</div>
+                <div className="weather">{weather.conditions}</div>
+                <div className="feels-like">
+                  Feels Like {weather.feelsLike}°c
+                </div>
+                <div className="high-low">
+                  {weather.high}°c / {weather.low}°c
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>
     </>
   );
