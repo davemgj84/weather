106e33dcb18171dcac201fa6976c323ed5d45ccb
David <davemgj@gmail.com>
2020-12-22 02:13:37 -0800

    update index.js - create netlify.toml file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-if (process.env.REACT_APP_API_BASE_URL) {
-  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-}
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
